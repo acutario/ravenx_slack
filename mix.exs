bc91b325e0642b60e93f893af9d7f8c6be55ff70
Óscar de Arriba <odarriba@gmail.com>
2018-02-14 21:05:05 +0100

    Update Mix info

--- mix.exs
+++ mix.exs
@@ -4,10 +4,14 @@ defmodule RavenxSlack.MixProject do
   def project do
     [
       app: :ravenx_slack,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.4",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      docs: docs()
     ]
   end
 
@@ -25,6 +29,30 @@ defmodule RavenxSlack.MixProject do
       {:poison, "~> 2.0 or ~> 3.0"},
       {:httpoison, "~> 0.12 or ~> 1.0"},
       {:credo, "~> 0.8", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      source_url: "https://github.com/acutario/ravenx_slack",
+      extras: ["README.md"]
+    ]
+  end
+
+  defp description do
+    """
+    Ravenx strategy to send notifications to Slack.
+    """
+  end
+
+  defp package do
+    # These are the default files included in the package
+    [
+      name: :ravenx_slack,
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Óscar de Arriba"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/acutario/ravenx_slack"}
     ]
   end
 end
