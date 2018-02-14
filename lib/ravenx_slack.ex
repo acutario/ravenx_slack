bacf4027218c849a461823be416e60063e053d6b
Óscar de Arriba <odarriba@gmail.com>
2018-02-14 20:23:12 +0100

    Add base code

---
+++ lib/ravenx_slack.ex
@@ -0,0 +1,18 @@
+defmodule RavenxSlack do
+  @moduledoc """
+  Documentation for RavenxSlack.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RavenxSlack.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ravenx_slack_test.exs
@@ -0,0 +1,8 @@
+defmodule RavenxSlackTest do
+  use ExUnit.Case
+  doctest RavenxSlack
+
+  test "greets the world" do
+    assert RavenxSlack.hello() == :world
+  end
+end
